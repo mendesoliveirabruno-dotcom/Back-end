6f3ff3b9e7422c018e6f5b30414825b03c6f4a05
mendesoliveirabruno-dotcom <mendes.oliveira.bruno@escola.pr.gov.br>
2026-03-09 08:59:37 -0300

    Create cont3.py

---
+++ Conteudo/cont3.py
@@ -0,0 +1,10 @@
+n = input('digite algo')
+print(n.isnumeric())# para perguntar se é um número
+print(n.isalpha())# para perguntar se é letra
+print(n.isalnum())# para perguntar se é letra ou número
+print(n.isupper())# para perguntar se é so com letras maisculas
+print(n.isdecimal())#para verificar se é decimal
+print(n.isdigit())#para veriicar se é um digito
+print(n.islower())#para verificar se é apenas letras minussculas
+print(n.isidentifier())#para veiicar se é um identificador valido
+print(n.isspace)#para verificar se tem espaço branco
