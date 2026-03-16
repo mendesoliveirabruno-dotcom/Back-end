ba4dbb6b55a53a294d0fe0437871ead384419470
mendesoliveirabruno-dotcom <mendes.oliveira.bruno@escola.pr.gov.br>
2026-03-16 10:33:36 -0300

    Create cont4.py

---
+++ Conteudo/cont4.py
@@ -0,0 +1,7 @@
+# o + é utilizado como operador de adição
+# o - é utilizado como sinal de subtração
+# o * é utilizado para fazer a multiplicação
+# / é utilizada para fazer a divisão
+# ** é utilizado para fazer a exponenciação
+# // é utilizado para fazer a divisão inteira
+# % é utilizada para o resto da divisão
