83e95600f75e9d0563b25a8ce018e280c144a0d0
mendesoliveirabruno-dotcom <mendes.oliveira.bruno@escola.pr.gov.br>
2026-03-26 08:20:57 -0300

    Create ex016.py

---
+++ EXERCICIO/ex016.py
@@ -0,0 +1,5 @@
+car = float(input("Digite a quantidade de dias que você usou o carro"))
+km = float(input("Digite quantos KMs você rodou com o carro"))
+vcar = car * 60
+vkm = km * 0.15
+print("A quantidade de KMs percorrido foi de {:.2f}, valor do carro é de: {:.2f}".format (vkm, vcar))
